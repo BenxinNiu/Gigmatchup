c4975f100f4f181c7f249d7e6fd227982f5d909e
Benxin Niu <bn2645@mun.ca>
2017-03-29 13:54:18 -0230

    initial

---
+++ server.js
@@ -0,0 +1,100 @@
+"use strict";
+"use strict";
+
+const express = require('express');
+const moment=require('moment');
+const mongo=require('mongodb').MongoClient;
+const assert=require('assert');
+const bodyparser=require('body-parser');
+const fs=require('fs');
+const session=require('express-session');
+const MemcachedStore = require('connect-memcached')(session);
+const passport = require('passport');
+const config = require('./config');
+const GoogleStrategy=require('passport-google-oauth20').Strategy;
+const path = require('path');
+//const oauth=require('./authentication')
+const app = express();
+
+//var mongoURL='mongodb://127.0.0.1:27017/test';
+const mongoURL='mongodb://benxinniu:nbx00144@ds137110.mlab.com:37110/testingdb';
+
+
+const sessionConfig={
+  resave: false,
+  saveUninitialized: false,
+  secret: config.get('SECRET'),
+  signed: true
+};
+
+if (config.get('NODE_ENV') === 'production') {
+  sessionConfig.store = new MemcachedStore({
+    hosts: [config.get('MEMCACHE_URL')]
+  });
+}
+
+function extractProfile(profile){
+let url='';
+if (profile.photos && profile.photos.length) {
+    url = profile.photos[0].value;
+  }
+  return {
+    id: profile.id,
+    displayName: profile.displayName,
+    image: url
+  };
+}
+
+function formatTime(date){
+  return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + "/" + date.getHours()+ ":" + date.getMinutes();
+}
+
+passport.use(new GoogleStrategy({
+  clientID: config.get('OAUTH2_CLIENT_ID'),
+  clientSecret: config.get('OAUTH2_CLIENT_SECRET'),
+  callbackURL: config.get('OAUTH2_CALLBACK'),
+  accessType: 'offline'
+},function(accessToken, refresh,profile, cb){
+  cb(null, extractProfile(profile));
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+
+
+//app.use(express.static('htmls/Ad'))
+app.use(session(sessionConfig));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json()); // for parsing application/json
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session(sessionConfig));
+
+//  This allows cross domain request
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+
+app.get('/auth/google',function (req,res,next){
+    next();
+},passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+app.get('/auth/google/callback',passport.authenticate('google'，{ failureRedirect: '/' }),function(req, res){
+     res.redirect('ad');
+  }
+);
+
+
+var server=app.listen(process.env.PORT || '8080', function(){
+  console.log('App is listening on the port %s', server.address().port);
+  console.log('You know what to do to quit the server')
+})

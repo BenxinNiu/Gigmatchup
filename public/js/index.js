2a5221abca5a1d9b4b51167688d33f1d6a9e6b41
Benxin Niu <bn2645@mun.ca>
2017-04-03 06:10:30 -0230

    update index page

--- public/js/index.js
+++ public/js/index.js
@@ -89,15 +89,77 @@ function is_login(){
 //  return url.slice(num);
 }
 
+function add_social_channel(channel){
+let id="#"+channel;
+switch(channel){
+case "fb":
+$(id).empty();
+$(id).append("<input id='facebook-url' class='urls' placeholder='facebook url'>");
+break;
+case "yt":
+$(id).empty();
+$(id).append("<input id='youtube_url' class='urls' placeholder='youtube url'>");
+break;
+case "tw":
+$(id).empty();
+$(id).append("<input id='twitter_url' class='urls' placeholder='twitter url'>");
+break;
+}
+}
+
+function start_post_ad(id){
+  switch(id){
+    case "get_started":
+    $('.post_ad').slideDown(500);
+    $('.navigate').empty();
+    break;
+    case "next_to_contact":
+    $('.b').animate({left:"1000px"},600)
+    $('.navigate').empty();
+    $('.navigate').append("<a id='go_back' class='btn btn-primary btn-lobster'>Go back</a>")
+      $('.navigate').append("<a id='post_now' class='btn btn-primary btn-lobster'>(2/2)Post Now</a>")
+    break;
+  }
+}
 
 // load some animation when the page is first loaded
 function initial_animation(){
   $('.service_detail').slideUp(0.1);
+  $('.post_ad').slideUp(0.1);
 }
+
+function optimize(){
+  var width=window.innerWidth;
+  if (width<=700)
+  $('.c').css('top','-600px')
+}
+
 
 $(document).ready(function(){
 
 initial_animation();
+
+optimize();
+
+$('.navigate').on('click','#post_now',function(){
+  post_ad();
+})
+
+$('.navigate').on('click','#go_back',function(){
+  $('.b').animate({left:"0px"},600)
+  $('.navigate').empty();
+  $('.navigate').append("<a id='next_to_contact' class='btn btn-primary btn-lobster'>(1/2)Next</a>")
+})
+
+$('.navigate').on('click','.btn',function(){
+var child=$(this).attr('id')
+start_post_ad(child);
+})
+
+$('.channel').on('click',function(){
+ let Id=$(this).parent().attr('id')
+  add_social_channel(Id);
+})
 
 $('.edit_profile_now').on('click',function(){
   if(is_login())
@@ -107,6 +169,7 @@ $('.edit_profile_now').on('click',function(){
 })
 $('.skip').on('click',function(){
   $('.a').animate({left:"1000px"},600)
+    $('.navigate').append("<a id='next_to_contact' class='btn btn-primary btn-lobster'>(1/2)Next</a>")
 })
 
 
@@ -133,8 +196,5 @@ $('.login_icon').on('click',function(){
       e.preventDefault();
       $("#sidebar-wrapper").toggleClass("active");
   });
-  //#to-top button appears after scrolling
-  var fixed = false;
 
-
-});
+}); // document

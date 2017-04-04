8377d56ee48e1016906a51ea0ce9e18dad7cc505
Benxin Niu <bn2645@mun.ca>
2017-04-04 14:30:46 -0230

    model_functions for login and ad posting

---
+++ mongoose_model/model.js
@@ -0,0 +1,38 @@
+const mongo=require('mongodb').MongoClient;
+
+function formatTime(date){
+  return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + "/" + date.getHours()+ ":" + date.getMinutes();
+}
+function construct_user(profile){
+  var date=new Date();
+  var formated=formatTime(date);
+return {
+  Oauth_ID: profile.id,
+  name:profile.displayName,
+  created_date:formated,
+  img_url:profile.image
+};
+}
+function construct_ad(infor,id){
+  var date=new Date();
+  var formated=formatTime(date);
+  return{
+  "ID":id,
+  "Oauth_ID":"null",
+  "title":infor.title,
+  "category":infor.category,
+  "description": infor.description,
+  "phone":infor.phone,
+  "email":infor.email,
+  "post_date":formated,
+  "facebook":infor.facebook,
+  "twitter":infor.twitter,
+  "youtube":infor.youtube,
+  "video":infor.video
+};
+}
+
+module.exports={
+construct:  construct_user,
+construct_ad: construct_ad
+};

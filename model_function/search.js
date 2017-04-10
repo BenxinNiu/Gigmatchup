f756ea5cbda0669b339aa23d1558d1f33303faaa
Benxin Niu <bn2645@mun.ca>
2017-04-10 18:16:48 -0230

    update

---
+++ model_function/search.js
@@ -0,0 +1,13 @@
+module.exports=function(keyword){
+var StringToArray=keyword.split('');
+var nums=0;
+for(var i=0;i<StringToArray.length;i++)
+if(typeof StringToArray[0]==='number')
+num++;
+
+if(num>=4){
+return 'ID';
+}
+
+
+}

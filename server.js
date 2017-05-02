dac5ddc1c30543ea158bd4f59d140adb79afd1a0
Benxin Niu <bn2645@mun.ca>
2017-05-02 15:03:55 -0230

    update

--- server.js
+++ server.js
@@ -416,9 +416,9 @@ app.get('/adinfor/:type',(req,res)=>{
           ad.find().toArray(function(err,docs){
             if(err){res.send(500); db.close();}
           for (var i=number;i<number+5&&i<docs.length;i++){
-              var data=docs[number];
-            // console.log(data);
+              var data=docs[i];
             result_array.unshift(data.snippet);
+            console.log(result_array)
           }
           db.close();
           res.send(result_array);
@@ -435,7 +435,7 @@ app.get('/adinfor/:type',(req,res)=>{
         }
         ad.find({"ID":type}).toArray(function(err,docs){
           if(err){res.send(500); db.close();}
-          for (var i=number;i<5&&i<docs.length;i++){
+          for (var i=number;i<number+5&&i<docs.length;i++){
               var data=docs[i];
               result_array.unshift(data.snippet);
           }
